6aed56df3065c9a6f5050ff2892db07af695b63d
Raphael Müller <raaphii@gmail.com>
2019-05-02 09:01:05 +0200

    All result pages should deliver base html

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var serveStatic = require('serve-static');
 
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
+app.use("/result/*", serveStatic(__dirname + "/dist/index.html")); // make sure all routes can be accessed
 var port = process.env.PORT || 5000;
 app.listen(port);
 
